7455263545a463c65eebc5496a7bd11982bd5de5
Florin Miron <mironflorin123@yahoo.com>
2025-05-20 19:51:51 +0100

    Install flask-bootstrap and initiate the home route

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_bootstrap import Bootstrap5
 import secrets
 
 secret_key = secrets.token_urlsafe(32)
 
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
 app.config['SECRET_KEY'] = secret_key
 
 class Base(DeclarativeBase):
@@ -16,5 +18,12 @@ class Base(DeclarativeBase):
 
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///cinedata.db'
 db = SQLAlchemy(model_class=Base)
-login_manager = LoginManager(app)
+# login_manager = LoginManager(app)
 db.init_app(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
