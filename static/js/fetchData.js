fe52b50aa93423b63a6b4d456fda77522af0ae68
Florin Miron <mironflorin123@yahoo.com>
2025-05-24 12:04:40 +0100

    Add the functionality for loading and displaying data

--- static/js/fetchData.js
+++ static/js/fetchData.js
@@ -1,10 +1,20 @@
+import {
+  renderSpinner,
+  hideSpinner,
+  displaySearchResults,
+} from "./renderData.js";
+
 const fetchData = async function (apiKey, inputValue) {
   try {
+    renderSpinner();
+
     const data = await fetch(
       `http://www.omdbapi.com/?apikey=${apiKey}&s=${inputValue}`
     );
     const response = await data.json();
-    console.log(response.Search);
+
+    hideSpinner();
+    displaySearchResults(response.Search);
   } catch (error) {
     console.log(error);
   }
