e7b7a7fbf6c2cce7e29923ee8900ea21ec3b4582
Florin Miron <mironflorin123@yahoo.com>
2025-05-24 12:57:19 +0100

    Change the script.js file name to login.js

---
+++ static/js/login.js
@@ -0,0 +1,22 @@
+const tabButtons = document.querySelectorAll(".tab-button");
+const tabContents = document.querySelectorAll(".tab-content");
+
+const handleTabClick = function () {
+  tabButtons.forEach((tabButton) => {
+    tabButton.addEventListener("click", () => {
+      const tabId = tabButton.getAttribute("data-tab");
+
+      tabButtons.forEach((tabButton) => {
+        tabButton.classList.remove("active");
+      });
+      tabContents.forEach((tabContent) => {
+        tabContent.classList.remove("active");
+      });
+      tabButton.classList.add("active");
+      const selectedTab = document.getElementById(tabId);
+      selectedTab.classList.add("active");
+    });
+  });
+};
+
+handleTabClick();
