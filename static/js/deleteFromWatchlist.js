dde9399c57719c7c63b2ec0f5af3c5921d3b4d27
Florin Miron <mironflorin123@yahoo.com>
2025-05-26 18:18:57 +0100

    Create the functionality for sending a delete request in the database and reflecting it in the dom

---
+++ static/js/deleteFromWatchlist.js
@@ -0,0 +1,41 @@
+const watchlistContainer = document.querySelector(".watchlist-container");
+const watchlistPlaceholder = document.querySelector(
+  ".watchlist-empty-paragraph"
+);
+
+const deleteFromWatchlistDB = async function (movieId) {
+  try {
+    const response = await fetch(`/delete_from_watchlist/${movieId}`, {
+      method: "DELETE",
+      credentials: "include",
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const handleDelteFromWatchlist = function () {
+  watchlistContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("remove-btn")) {
+      const removeBtn = e.target;
+      const movieEl = removeBtn.closest("[data-movie-id]");
+      const movieId = movieEl.dataset.movieId;
+
+      [...watchlistContainer.children].filter((movie) => {
+        movie.dataset.movieId !== movieId;
+      });
+
+      movieEl.remove();
+
+      if ([...watchlistContainer.children].length === 1) {
+        watchlistPlaceholder.classList.remove("d-none");
+      }
+
+      deleteFromWatchlistDB(movieId);
+    }
+  });
+};
+
+export default handleDelteFromWatchlist;
