83b7f7a8124689ff10f6ff07468f3d9264929dac
Florin Miron <mironflorin123@yahoo.com>
2025-05-27 12:53:23 +0100

    Create the functionality for fetching the rated movies from the database as the user logs in

--- static/js/login.js
+++ static/js/login.js
@@ -1,10 +1,12 @@
 import { renderWatchlistItem } from "./renderWatchlistItem.js";
+import { renderRatingItem } from "./renderRatingItem.js";
 
 const watchlistPlaceholder = document.querySelector(
   ".watchlist-empty-paragraph"
 );
+const ratingsPlaceholder = document.querySelector(".ratings-empty-paragraph");
 
-const fetchStoredMovies = async function () {
+const fetchWatchlistMovies = async function () {
   try {
     const response = await fetch("/watchlist");
     const data = await response.json();
@@ -20,9 +22,28 @@ const fetchStoredMovies = async function () {
   }
 };
 
+const fetchRatedMovies = async function () {
+  try {
+    const response = await fetch("/ratings");
+    const data = await response.json();
+
+    if (data.length > 0) {
+      ratingsPlaceholder.classList.add("d-none");
+      for (const movieItem of data) {
+        console.log(movieItem);
+
+        renderRatingItem(movieItem, movieItem.userRating);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const handleLoadStoredMovies = function () {
   document.addEventListener("DOMContentLoaded", () => {
-    fetchStoredMovies();
+    fetchWatchlistMovies();
+    fetchRatedMovies();
   });
 };
 
