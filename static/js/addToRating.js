ded4aa6d31d273e3d7b5a6a5b3ead83a7afbce0e
Florin Miron <mironflorin123@yahoo.com>
2025-05-27 11:03:35 +0100

    Implement the functionality for adding the movie rating to database

--- static/js/addToRating.js
+++ static/js/addToRating.js
@@ -5,6 +5,28 @@ import displayToast from "./displayToast.js";
 
 const submitRatingBtn = document.querySelector(".submit-rating-btn");
 
+const addToRatingDB = async function (fetchedData, userRating) {
+  const movieData = {
+    title: fetchedData.Title,
+    release_date: fetchedData.Released,
+    poster_url: fetchedData.Poster,
+    movie_id: fetchedData.imdbID,
+    user_rating: userRating,
+  };
+
+  try {
+    const response = await fetch(`/add_to_rating`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(movieData),
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const handleAddToRating = function () {
   // Use an anonymous function instead of arrow function to avoid losing the value of the 'this' keyword
   submitRatingBtn.addEventListener("click", function () {
@@ -38,3 +60,4 @@ const handleAddToRating = function () {
 };
 
 export default handleAddToRating;
+export { addToRatingDB };
